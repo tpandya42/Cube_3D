e761eb294ee80a5cc59329812f986f1e605cdade
Tanmay <tanmaypandya159@gmail.com>
2025-11-07 09:25:39 +0100

    setting up basic structure of directory and its potential development

---
+++ include/cub.h
@@ -0,0 +1,15 @@
+#ifndef CUB_H
+#define CUB_H
+
+#include <unstd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+
+// Include Sub Headers
+#include "stuct.h"
+
+#endif
---
+++ include/struct.h
@@ -0,0 +1,10 @@
+#ifndef STRUCT_H
+# define STRUCT_H
+
+#include "cub.h"
+
+// DEFINE STRUCTS
+
+
+
+#endif
