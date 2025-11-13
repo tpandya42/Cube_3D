05f1dfb8d52c46de24ebe0ef49de3edffab81916
mainibet <mainibet@gmail.com>
2025-11-13 16:22:11 +0100

    General cleanup in process

--- src/utils/cleanup.c
+++ src/utils/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 15:09:53 by albetanc          #+#    #+#             */
-/*   Updated: 2025/11/13 15:10:17 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/11/13 16:21:53 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	clean_exit(t_display *data, int status)
 {
 	if (data->map && data->map->grid)
 		free_array(data->map->grid, data->map->rows);
-	//clean_textures(data);
+	//clean_textures(game->data);
 	if (data->win)
 		mlx_destroy_window(data->mlx, data->win);
 	if (data->mlx)
