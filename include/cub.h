c9d0d1b7bec316146ad3bba5caab2b407bb100cf
mainibet <mainibet@gmail.com>
2025-11-14 08:09:32 +0100

    own headers

--- include/cub.h
+++ include/cub.h
@@ -1,15 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 07:47:45 by albetanc          #+#    #+#             */
+/*   Updated: 2025/11/14 08:03:39 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB_H
-#define CUB_H
+# define CUB_H
 
-#include <unstd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <stdbool.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/time.h>
+# include <stdbool.h>
 
+// Own headers
+# include "struct.h"
+# include "raycast.h"
+# include "util.h"
 
-// Include Sub Headers
-#include "stuct.h"
+//Bonus headers
+# include "minimap_bonus.h"
+
+//Debug headers
+# include "debug.h"
 
 #endif
